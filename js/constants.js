175b4f4c97a53ec03019efb45a1dbf607203217c
Tony Stark <138078424+TonyStarkBotz@users.noreply.github.com>
2025-06-15 14:57:44 +0530

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,4 @@
 
-const API_KEY = 'YOUR_TMDB_API_KEY';
+const API_KEY = 'bf388abad0e6e262e3fdd564f4a91c82';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
